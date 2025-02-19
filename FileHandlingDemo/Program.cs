dfda098c1b50c76f2687d81106dfa3e129f6e235
aayushhman <aayushmandal696@gmail.com>
2025-02-19 11:37:32 +0545

    Add project files.

---
+++ FileHandlingDemo/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.IO;
+
+namespace FileHandlingDemo
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string path = "D:\\data.txt";
+            if (File.Exists(path))
+            {
+                Console.WriteLine("this file is found..");
+            }
+            else
+            {
+                Console.WriteLine("This fiole is not found..");
+            }
+            Console.ReadLine();
+        }
+    }
+}
